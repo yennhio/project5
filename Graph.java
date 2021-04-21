6c99562935f2b8c38795769f34bda6422e31635c
Yennhi Dang <nhidangn@gmail.com>
2021-04-20 19:59:04 -0700

    more default methods

--- Graph.java
+++ Graph.java
@@ -8,4 +8,16 @@ public class Graph<E> {
         edges = new boolean[n][n];
         labels = (E[]) new Object[n];
     }
+
+    public E getLabel (int vertex) {
+        return labels[vertex];
+    }
+
+    public boolean isEdge(int source, int target) {
+        return edges[source][target];
+    }
+
+    public void addEdge(int source, int target) {
+        edges[source][target] = true;
+    }
 }
