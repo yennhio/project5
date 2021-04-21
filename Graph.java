130f0b7a1c102b8c43879417cf51dc2c26fd68de
Yennhi Dang <nhidangn@gmail.com>
2021-04-20 19:57:06 -0700

    first constructor

--- Graph.java
+++ Graph.java
@@ -1,5 +1,11 @@
 
 
 public class Graph<E> {
-    
+    private boolean [][] edges;
+    private E[] labels;
+
+    public Graph (int n) {
+        edges = new boolean[n][n];
+        labels = (E[]) new Object[n];
+    }
 }
