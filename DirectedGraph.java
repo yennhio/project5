bf62d9381b6f64db8e4f31111b8bde56160f2112
Yennhi Dang <nhidangn@gmail.com>
2021-04-23 10:52:49 -0700

    implemented more GraphInterface methods

--- DirectedGraph.java
+++ DirectedGraph.java
@@ -54,5 +54,24 @@ public class DirectedGraph<T> implements GraphInterface<T>
    return found;
 }
 
+public boolean isEmpty()
+{
+   return vertices.isEmpty();
+}
 
+public void clear()
+{
+   vertices.clear();
+   edgeCount = 0;
+} // end clear
+
+public int getNumberOfVertices()
+{
+   return vertices.getSize();
+} // end getNumberOfVertices
+
+public int getNumberOfEdges()
+{
+   return edgeCount;
+} 
 } // end DirectedGraph
