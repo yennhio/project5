6b14ffcb4529997baafef9509667e09f16967ed1
Yennhi Dang <nhidangn@gmail.com>
2021-04-23 10:55:07 -0700

    fixed indentation in DirectedGraph.java

--- DirectedGraph.java
+++ DirectedGraph.java
@@ -21,14 +21,15 @@ public class DirectedGraph<T> implements GraphInterface<T>
 
     public boolean addEdge(T begin, T end, double edgeWeight)
     {
-   boolean result = false;
-   VertexInterface<T> beginVertex = vertices.getValue(begin);
-   VertexInterface<T> endVertex = vertices.getValue(end);
-   if ( (beginVertex != null) && (endVertex != null) )
-      result = beginVertex.connect(endVertex, edgeWeight);
-   if (result)
-      edgeCount++;
-   return result;
+        boolean result = false;
+        VertexInterface<T> beginVertex = vertices.getValue(begin);
+        VertexInterface<T> endVertex = vertices.getValue(end);
+        if ( (beginVertex != null) && (endVertex != null) )
+            result = beginVertex.connect(endVertex, edgeWeight);
+        if (result)
+            edgeCount++;
+        
+        return result;
     }
 
     public boolean addEdge(T begin, T end)
@@ -37,41 +38,42 @@ public class DirectedGraph<T> implements GraphInterface<T>
     }
 
     public boolean hasEdge(T begin, T end)
-{
-   boolean found = false;
-   VertexInterface<T> beginVertex = vertices.getValue(begin);
-   VertexInterface<T> endVertex = vertices.getValue(end);
-   if ( (beginVertex != null) && (endVertex != null) )
-   {
-      Iterator<VertexInterface<T>> neighbors = beginVertex.getNeighborIterator();
-      while (!found && neighbors.hasNext())
-      {
-         VertexInterface<T> nextNeighbor = neighbors.next();
-         if (endVertex.equals(nextNeighbor))
-            found = true;
-      } // end while
-   } // end if
-   return found;
-}
+    {
+        boolean found = false;
+        VertexInterface<T> beginVertex = vertices.getValue(begin);
+        VertexInterface<T> endVertex = vertices.getValue(end);
+        if ( (beginVertex != null) && (endVertex != null) )
+        {
+            Iterator<VertexInterface<T>> neighbors = beginVertex.getNeighborIterator();
+            while (!found && neighbors.hasNext())
+            {
+                VertexInterface<T> nextNeighbor = neighbors.next();
+                if (endVertex.equals(nextNeighbor))
+                    found = true;
+            } // end while
+        } // end if
+        return found;
+    }
 
-public boolean isEmpty()
-{
-   return vertices.isEmpty();
-}
+    public boolean isEmpty()
+    {
+        return vertices.isEmpty();
+    }
 
-public void clear()
-{
-   vertices.clear();
-   edgeCount = 0;
-} // end clear
+    public void clear()
+    {
+        vertices.clear();
+        edgeCount = 0;
+    } // end clear
 
-public int getNumberOfVertices()
-{
-   return vertices.getSize();
-} // end getNumberOfVertices
+    public int getNumberOfVertices()
+    {
+        return vertices.getSize();
+    } // end getNumberOfVertices
 
-public int getNumberOfEdges()
-{
-   return edgeCount;
-} 
+    public int getNumberOfEdges()
+    {
+        return edgeCount;
+    } 
+    
 } // end DirectedGraph
