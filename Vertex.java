5e1517b6e05cf6a6307b44568a11baa2cd18d073
Yennhi Dang <nhidangn@gmail.com>
2021-04-23 14:44:53 -0700

    added connect methods

--- Vertex.java
+++ Vertex.java
@@ -19,6 +19,40 @@ public class Vertex<T> implements VertexInterface<T> {
        cost = 0;
     }
 
+
+public boolean connect(VertexInterface<T> endVertex, double edgeWeight)
+{
+   boolean result = false;
+
+   if (!this.equals(endVertex))
+   {  // Vertices are distinct
+      Iterator<VertexInterface<T>> neighbors = getNeighborIterator();
+      boolean duplicateEdge = false;
+
+      while (!duplicateEdge && neighbors.hasNext())
+      {
+         VertexInterface<T> nextNeighbor = neighbors.next();
+         if (endVertex.equals(nextNeighbor))
+            duplicateEdge = true;
+      } // end while
+
+      if (!duplicateEdge)
+      {
+         edgeList.add(new Edge(endVertex, edgeWeight));
+         result = true;
+      } // end if
+   } // end if
+
+   return result;
+} // end connect
+
+public boolean connect(VertexInterface<T> endVertex)
+{
+   return connect(endVertex, 0);
+} // end connect
+
+
+
 protected class Edge
 {
    private VertexInterface<T> vertex; // Vertex at end of edge
