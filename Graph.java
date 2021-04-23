a794eb0722351f9223a3887c478f100c8fce2d93
Yennhi Dang <nhidangn@gmail.com>
2021-04-23 12:49:10 -0700

    added traversal methods

--- Graph.java
+++ Graph.java
@@ -59,53 +59,7 @@ public class Graph<E> {
         return labels.length;
     }
 
-    public QueueInterface<T> getBreadthFirstTraversal(T origin) {
-        resetVertices();
-        QueueInterface<T> traversalOrder = new LinkedQueue<T>();
-        QueueInterface<VertexInterface<T>> vertexQueue = new LinkedQueue<VertexInterface<T>>();
-        VertexInterface<T> originVertex = vertices.getValue(origin);
-        originVertex.visit();
-        traversalOrder.enqueue(origin);
-        vertexQueue.enqueue(originVertex);
-
-        while (!vertexQueue.isEmpty()) {
-            VertexInterface<T> frontVertex = vertexQueue.dequeue();
-            Iterator<VertexInterface<T>> neighbors = frontVertex.getNeighborIterator();
-
-            while (neighbors.hasNext()) {
-                VertexInterface<T> nextNeighbor = neighbors.next();
-                if (!nextNeighbor.isVisisted()) {
-                    nextNeighbor.visit();
-                    traversalOrder.enqueue(nextNeighbor.getLabel());
-                    vertexQueue.enqueue(nextNeighbor);
-                }
-            }
-        }
-        return traversalOrder;
-    }
+    
    
-    public QueueInterface<T> getDepthFirstTraversal(T origin) {
-        resetVertices();
-        QueueInterface<T> traversalOrder = new LinkedQueue<T>();
-        StackInterface<VertexInterface<T>> vertexStack = new LinkedStack<>();
-
-        VertexInterface<T> originVertex = vertices.getValue(origin);
-        originVertex.visit();
-        traversalOrder.enqueue(origin);
-        vertexStack.push(originVertex);
-
-        while (!vertexStack.isEmpty()) {
-            VertexInterface<T> topVertex = vertexStack.peek();
-            VertexInterface<T> nextNeighbor = topVertex.getUnvisitedNeighbor();
-
-            if (nextNeighbor != null) {
-                nextNeighbor.visit();
-                traversalOrder.enqueue(nextNeighbor.getLabel());
-                vertexStack.push(nextNeighbor);
-            }
-            else
-                vertexStack.pop();
-        }
-        return traversalOrder;
-    }
+    
 }
