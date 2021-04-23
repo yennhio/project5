a0980a957aa273d2dc3587b0e5b78531d775d0eb
Yennhi Dang <nhidangn@gmail.com>
2021-04-23 10:22:39 -0700

    depth first method

--- Graph.java
+++ Graph.java
@@ -84,4 +84,28 @@ public class Graph<E> {
         return traversalOrder;
     }
    
+    public QueueInterface<T> getDepthFirstTraversal(T origin) {
+        resetVertices();
+        QueueInterface<T> traversalOrder = new LinkedQueue<T>();
+        StackInterface<VertexInterface<T>> vertexStack = new LinkedStack<>();
+
+        VertexInterface<T> originVertex = vertices.getValue(origin);
+        originVertex.visit();
+        traversalOrder.enqueue(origin);
+        vertexStack.push(originVertex);
+
+        while (!vertexStack.isEmpty()) {
+            VertexInterface<T> topVertex = vertexStack.peek();
+            VertexInterface<T> nextNeighbor = topVertex.getUnvisitedNeighbor();
+
+            if (nextNeighbor != null) {
+                nextNeighbor.visit();
+                traversalOrder.enqueue(nextNeighbor.getLabel());
+                vertexStack.push(nextNeighbor);
+            }
+            else
+                vertexStack.pop();
+        }
+        return traversalOrder;
+    }
 }
