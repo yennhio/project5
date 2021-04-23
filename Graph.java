361bec8bed12ba77d80f8886f7c6b8f1ab2e5041
Yennhi Dang <nhidangn@gmail.com>
2021-04-23 10:18:17 -0700

    breadth first method added

--- Graph.java
+++ Graph.java
@@ -61,6 +61,27 @@ public class Graph<E> {
 
     public QueueInterface<T> getBreadthFirstTraversal(T origin) {
         resetVertices();
-        
+        QueueInterface<T> traversalOrder = new LinkedQueue<T>();
+        QueueInterface<VertexInterface<T>> vertexQueue = new LinkedQueue<VertexInterface<T>>();
+        VertexInterface<T> originVertex = vertices.getValue(origin);
+        originVertex.visit();
+        traversalOrder.enqueue(origin);
+        vertexQueue.enqueue(originVertex);
+
+        while (!vertexQueue.isEmpty()) {
+            VertexInterface<T> frontVertex = vertexQueue.dequeue();
+            Iterator<VertexInterface<T>> neighbors = frontVertex.getNeighborIterator();
+
+            while (neighbors.hasNext()) {
+                VertexInterface<T> nextNeighbor = neighbors.next();
+                if (!nextNeighbor.isVisisted()) {
+                    nextNeighbor.visit();
+                    traversalOrder.enqueue(nextNeighbor.getLabel());
+                    vertexQueue.enqueue(nextNeighbor);
+                }
+            }
+        }
+        return traversalOrder;
     }
+   
 }
