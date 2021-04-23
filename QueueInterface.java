1509ea562e6997a62efc4adbda39f2a1edcc6977
Yennhi Dang <nhidangn@gmail.com>
2021-04-23 13:19:58 -0700

    added queue interface

---
+++ QueueInterface.java
@@ -0,0 +1,11 @@
+public interface QueueInterface<T> {
+    public void enqueue(T newEntry);
+
+    public T dequeue();
+
+    public T getFront();
+
+    public boolean isEmpty();
+
+    public void clear();
+}
