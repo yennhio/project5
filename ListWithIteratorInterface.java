894700390520b3c85f9c1784aa38309f89a5fb48
Yennhi Dang <nhidangn@gmail.com>
2021-04-23 17:21:18 -0700

    implemented lots of methods

---
+++ ListWithIteratorInterface.java
@@ -0,0 +1,6 @@
+import java.util.Iterator;
+
+public interface ListWithIteratorInterface<T> extends ListInterface<T>, Iterable<T>
+{
+   public Iterator<T> getIterator();
+} // end ListWithIteratorInterface
