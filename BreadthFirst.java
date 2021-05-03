84abce7b688633ec91abc6d8b495782ddfa994d7
Yennhi Dang <nhidangn@gmail.com>
2021-05-02 17:17:04 -0700

    add edge method

--- BreadthFirst.java
+++ BreadthFirst.java
@@ -6,11 +6,18 @@ public class BreadthFirst {
     private int vertices;   // No. of vertices
     private LinkedList<Integer> adj[]; //Adjacency Lists
 
+    /** Constructor */
     public BreadthFirst(int verts)
     {
         vertices = verts;
         adj = new LinkedList[verts];
         for (int i=0; i<verts; ++i)
             adj[i] = new LinkedList();
+    }
+
+    /** Adds an edge to the graph */
+    public void addEdge(int v,int w)
+    {
+        adj[v].add(w);
     }
 }
