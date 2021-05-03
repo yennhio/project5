224dd123a18c79d6575b9f09708405e37a645310
Yennhi Dang <nhidangn@gmail.com>
2021-05-02 17:23:35 -0700

    depth first constructor

--- DepthFirst.java
+++ DepthFirst.java
@@ -1,3 +1,18 @@
 import java.io.*;
 import java.util.*;
 
+public class DepthFirst {
+   private int vertices; // No. of vertices
+
+   // Adjacency List Representation
+   private LinkedList<Integer> adj[];
+
+   /** Constructor */
+   @SuppressWarnings("unchecked") DepthFirst(int verts)
+    {
+        vertices = verts;
+        adj = new LinkedList[verts];
+        for (int i = 0; i < verts; ++i)
+            adj[i] = new LinkedList();
+    }
+}
