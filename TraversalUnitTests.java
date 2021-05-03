88e0ec33041c177ea5720db57b63776c3025de70
Yennhi Dang <nhidangn@gmail.com>
2021-05-02 17:34:57 -0700

    added unit tests for breadth first

--- TraversalUnitTests.java
+++ TraversalUnitTests.java
@@ -2,12 +2,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.Test;
 
-public class TraversalUnitTests {
+public class BreadthUnitTests {
     
     BreadthFirst test = new BreadthFirst(9);
 
     @Test
     public void testAddEdge() {
         test.addEdge(0, 1);
+    }
+
+    @Test
+    public void testBFS() {
+        test.addEdge(0, 1);
+        test.addEdge(0, 2);
+        test.addEdge(0, 3);
+        test.addEdge(2, 3);
+        test.addEdge(3, 4);
+        test.addEdge(2, 5);
+
+        test.BFS(0);
     }
 }
